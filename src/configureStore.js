9508cc6dccacf89b98f2ce539615eff79fb28d6e
Md Kamrul Islam <kamruli@gmail.com>
2019-10-16 08:54:07 +1100

    redux store configured

---
+++ src/configureStore.js
@@ -0,0 +1,23 @@
+import { applyMiddleware, createStore } from 'redux';
+// import thunkMiddleware from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga'
+
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import monitorReducersEnhancer from './enhancers/monitorReducers';
+import loggerMiddleware from './middleware/logger';
+import rootReducer from './reducers/index';
+
+export default function configureStore(preloadedState) {
+  const sagaMiddleware = createSagaMiddleware()
+  const middlewares = [loggerMiddleware, sagaMiddleware];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
+
+  const enhancers = [middlewareEnhancer, monitorReducersEnhancer];
+  // const composedEnhancers = compose(...enhancers);
+  const composedEnhancers = composeWithDevTools(...enhancers);
+
+  const store = createStore(rootReducer, preloadedState, composedEnhancers);
+
+  return store;
+};
