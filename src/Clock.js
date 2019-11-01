037d60d994e4652cf59ddb1b426269de58973bda
Md Kamrul Islam <kamruli@gmail.com>
2019-11-01 19:00:23 +1100

    dynamic clock.svg preparation

--- src/Clock.js
+++ src/Clock.js
@@ -7,13 +7,13 @@ const Clock = () => {
         width: '400px',
         fill: 'white',
         stroke: 'black',
-        'stroke-width': 1,
-        'stroke-linecap': 'round',
+        strokeWidth: 1,
+        strokeLinecap: 'round',
         transform: 'rotate(-90deg)'
     };
     const tiaText = {
-        'font-size': '1px',
-        'font-family': 'sans-serif',
+        fontSize: '1px',
+        fontFamily: 'sans-serif',
         transform: 'translate(14px, 19px) rotate(90deg)',
         fill: '#dfdfdf',
         stroke: 'none'
@@ -21,7 +21,7 @@ const Clock = () => {
     return <div >
     <svg style={svgStyle} viewBox="0 0 40 40">
       <circle cx="20" cy="20" r="19" />
-      <g class="marks">
+      <g className="marks">
         <line x1="15" y1="0" x2="16" y2="0" />
         <line x1="15" y1="0" x2="16" y2="0" />
         <line x1="15" y1="0" x2="16" y2="0" />
@@ -36,11 +36,11 @@ const Clock = () => {
         <line x1="15" y1="0" x2="16" y2="0" />
         <line x1="15" y1="0" x2="16" y2="0" />
       </g>
-      <line x1="0" y1="0" x2="9" y2="0" class="hour" />
-      <line x1="0" y1="0" x2="13" y2="0" class="minute" />
+      <line x1="0" y1="0" x2="9" y2="0" className="hour" />
+      <line x1="0" y1="0" x2="13" y2="0" className="minute" />
       {/* <line x1="0" y1="0" x2="16" y2="0" class="seconds" /> */}
-      <circle cx="20" cy="20" r="0.7" class="pin" />
-      <text style={tiaText} x="0" y="0" class="tiaText">#TIA</text>
+      <circle cx="20" cy="20" r="0.7" className="pin" />
+      <text style={tiaText} x="0" y="0" className="tiaText">#TIA</text>
     </svg>
 
   </div>
--- src/ClockSvg.js
+++ src/ClockSvg.js
@@ -1,38 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 const ClockSvg = () => {
-    // const svgStyle = {
-    //     width: '400px',
-    //     fill: 'white',
-    //     stroke: 'black',
-    //     'stroke-width': 1,
-    //     'stroke-linecap': 'round',
-    //     transform: 'rotate(-90deg)'
-    // };
-    // const tiaText = {
-    //     'font-size': '1px',
-    //     'font-family': 'sans-serif',
-    //     transform: 'translate(14px, 19px) rotate(90deg)',
-    //     fill: '#dfdfdf',
-    //     stroke: 'none'
-    // };
+
+    const [hoursMark, setHoursMark] = useState([
+        {
+            deg: 30,
+            stroke: 0.2
+        },
+        {
+            deg: 60,
+            stroke: 0.2
+        },
+        {
+            deg: 90,
+            stroke: 0.5
+        },
+        {
+            deg: 120,
+            stroke: 0.2
+        },
+        {
+            deg: 150,
+            stroke: 0.2
+        },
+        {
+            deg: 180,
+            stroke: 0.5
+        },
+        {
+            deg: 210,
+            stroke: 0.2
+        },
+        {
+            deg: 240,
+            stroke: 0.2
+        },
+        {
+            deg: 270,
+            stroke: 0.5
+        },
+        {
+            deg: 300,
+            stroke: 0.2
+        },
+        {
+            deg: 330,
+            stroke: 0.2
+        },
+        {
+            deg: 360,
+            stroke: 0.5
+        },
+    ]);
+
     return <div >
     <svg 
         width="400" 
         fill="white" 
         stroke="black" 
-        stroke-width="1" 
-        stroke-linecap="round"
+        strokeWidth="1" 
+        strokeLinecap="round"
         viewBox="0 0 40 40">
       
       <circle cx="20" cy="20" r="19" />
-      <g class="marks-svg" 
+      <g className="marks-svg" 
         transform="translate(20, 20)"
-        stroke-width="0.2"
+        strokeWidth="0.2"
       >
-        <line transform="rotate(30)" x1="15" y1="0" x2="16" y2="0" />
+        {
+           hoursMark.map((hour, i)=> <line key={i} transform={'rotate(' + hour.deg + ')'} strokeWidth={hour.stroke} x1="15" y1="0" x2="16" y2="0" />) 
+        }  
+        {/* <line transform="rotate(30)" x1="15" y1="0" x2="16" y2="0" />
         <line transform="rotate(60)" x1="15" y1="0" x2="16" y2="0" />
         <line transform="rotate(90)" stroke-width="0.5" x1="15" y1="0" x2="16" y2="0" />
         <line transform="rotate(120)" x1="15" y1="0" x2="16" y2="0" />
@@ -43,14 +83,14 @@ const ClockSvg = () => {
         <line transform="rotate(270)" stroke-width="0.5" x1="15" y1="0" x2="16" y2="0" />
         <line transform="rotate(300)" x1="15" y1="0" x2="16" y2="0" />
         <line transform="rotate(330)" x1="15" y1="0" x2="16" y2="0" />
-        <line transform="rotate(360)" stroke-width="0.5" x1="15" y1="0" x2="16" y2="0" />
+        <line transform="rotate(360)" stroke-width="0.5" x1="15" y1="0" x2="16" y2="0" /> */}
         {/* <line transform="rotate(30)" x1="15" y1="0" x2="16" y2="0" /> */}
       </g>
-      <line transform="translate(20, 20) rotate(330)" stroke-width="1" x1="0" y1="0" x2="9" y2="0" class="hour-svg" />
-      <line transform="translate(20, 20) rotate(270)" stroke-width="0.6" x1="0" y1="0" x2="13" y2="0" class="minute-svg" />
+      <line transform="translate(20, 20) rotate(330)" strokeWidth="1" x1="0" y1="0" x2="9" y2="0" className="hour-svg" />
+      <line transform="translate(20, 20) rotate(270)" strokeWidth="0.6" x1="0" y1="0" x2="13" y2="0" className="minute-svg" />
       {/* <line x1="0" y1="0" x2="16" y2="0" class="seconds" /> */}
-      <circle stroke="#d00505" stroke-width="0.2" cx="20" cy="20" r="0.7" class="pin-svg" />
-      <text  x="0" y="0" class="tiaText-svg">#TIA</text>
+      <circle stroke="#d00505" strokeWidth="0.2" cx="20" cy="20" r="0.7" className="pin-svg" />
+      {/* <text  x="0" y="0" class="tiaText-svg">#TIA</text> */}
     </svg>
 
   </div>
