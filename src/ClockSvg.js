86b7db3a78240cb70d211cd1de2ff33668dc5cfe
Md Kamrul Islam <kamruli@gmail.com>
2019-11-02 16:32:38 +1100

    showing dynamic svg clock

--- src/ClockSvg.js
+++ src/ClockSvg.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState,  useEffect } from 'react';
 // import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 const ClockSvg = () => {
 
-    const [hoursMark, setHoursMark] = useState([
+    const [hoursMark] = useState([
         {
             deg: 30,
             stroke: 0.2
@@ -55,6 +55,25 @@ const ClockSvg = () => {
         },
     ]);
 
+    const [time, setTime] = useState({
+        hourHand: (((new Date()).getHours() % 12) * 30) + Math.floor((new Date()).getMinutes() / 2) + 270,
+        minHand: (new Date().getMinutes()) * 6 + 270  
+    });
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            console.log(
+                'executing : ' + new Date()
+            );
+            setTime({
+                hourHand: (((new Date()).getHours() % 12) * 30) + Math.floor((new Date()).getMinutes() / 2) + 270,
+                minHand: (new Date().getMinutes()) * 6 + 270      
+            });
+        }, 60000);
+        return () => clearInterval(interval);
+      }, []);
+
+
     return <div >
     <svg 
         width="400" 
@@ -73,8 +92,8 @@ const ClockSvg = () => {
            hoursMark.map((hour, i)=> <line key={i} transform={'rotate(' + hour.deg + ')'} strokeWidth={hour.stroke} x1="15" y1="0" x2="16" y2="0" />) 
         }  
       </g>
-      <line transform="translate(20, 20) rotate(330)" strokeWidth="1" x1="0" y1="0" x2="9" y2="0" className="hour-svg" />
-      <line transform="translate(20, 20) rotate(270)" strokeWidth="0.6" x1="0" y1="0" x2="13" y2="0" className="minute-svg" />
+      <line transform={"translate(20, 20) rotate(" + time.hourHand + ")"} strokeWidth="1" x1="0" y1="0" x2="9" y2="0" className="hour-svg" />
+      <line transform={"translate(20, 20) rotate(" + time.minHand + ")"} strokeWidth="0.6" x1="0" y1="0" x2="13" y2="0" className="minute-svg" />
       {/* <line x1="0" y1="0" x2="16" y2="0" class="seconds" /> */}
       <circle stroke="#d00505" strokeWidth="0.2" cx="20" cy="20" r="0.7" className="pin-svg" />
       {/* <text  x="0" y="0" class="tiaText-svg">#TIA</text> */}
