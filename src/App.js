6217500c3dfc76207787796575364bc4205de8a1
Md Kamrul Islam <kamruli@gmail.com>
2019-11-25 08:49:21 +1100

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import './App.css';
 
-import Counter from './Counter'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';  
+import Counter from './Counter';
 import store from './configureStore';
 import Clock from './Clock';
 import ClockSvg from './ClockSvg';
@@ -14,18 +20,38 @@ function App() {
   
   return (
     <div className="App App-background">
-      <header className="App-header">
-        React Base App
-      </header>
-      <Counter 
-        value={store.getState().counter}
-        onIncrement={() => action('INCREMENT')}
-        onDecrement={() => action('DECREMENT')}
-        onIncrementAsync={() => action('INCREMENT_ASYNC')}
-      />
-      
-      <Clock />
-      <ClockSvg />
+      <Router>
+        <header className="App-header">
+          React Base App
+          <ul>
+            <li>
+              <Link to="/clock">Clock</Link>
+            </li>
+            <li>
+              <Link to="/clock-quiz">Clock Quiz</Link>
+            </li>
+        </ul>
+        </header>
+        <Switch>
+          <Route path="/clock">
+            <ClockSvg />
+          </Route>
+          <Route path="/clock-quiz">
+            <Counter 
+              value={store.getState().counter}
+              onIncrement={() => action('INCREMENT')}
+              onDecrement={() => action('DECREMENT')}
+              onIncrementAsync={() => action('INCREMENT_ASYNC')}
+            />
+          </Route>
+          {/* <PrivateRoute path="/protected">
+            <ProtectedPage />
+          </PrivateRoute> */}
+        </Switch>
+        {/* <Clock /> */}
+        {/* <ClockSvg /> */}
+      </Router>
+
     </div>
   );
 }
